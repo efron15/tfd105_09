1f07286c8e83ae32b85cb31aa29398294e778dee
efron15 <fundate0115@gmail.com>
2022-02-15 11:56:12 +0800

    暫時更改

---
+++ scripts/owlCarousel.js
@@ -0,0 +1,27 @@
+$(".owl-carousel").owlCarousel({
+    loop: true, // 循環播放
+    margin: 0, // 外距 10px
+    nav: false, // 顯示點點
+    autoplay: true, // 自動輪播
+    autoplaySpeed: 1000,
+    autoplayTimeout: 3000, // 切換時間
+    autoplayHoverPause: true, // 滑鼠經過時暫停
+    responsive: {
+        0: {
+        items: 1 // 螢幕大小為 0~600 顯示 1 個項目
+        },
+        600: {
+        items: 1 // 螢幕大小為 600~1000 顯示 3 個項目
+        },
+        1000: {
+        items: 1 // 螢幕大小為 1000 以上 顯示 5 個項目
+        }
+    }
+});
+
+var dots = $('.owl-dots').css('position', 'absolute').css('bottom', '5px');
+dots.css('left', 'calc(50% - ' + dots.width()/2+'px)');
+
+// 文字輪播
+$('#J_scroll').width($('#J_scroll').width() - $('#J_scroll li:first-child').innerWidth());
+$('#J_scroll').addClass('theanimation');
