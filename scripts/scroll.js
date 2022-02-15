d61c10e3d65484e9220d7451927fad5951c94d24
efron15 <fundate0115@gmail.com>
2022-02-15 09:26:45 +0800

    修改首頁用滾動選單JQ ,首頁輪播照片加入   3.4還未新增

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -8,3 +8,15 @@ $(window).scroll(function(){
         $('#blue_menu').css('display','none');
     }
 });
+
+// 首頁使用的滾動
+$(window).scroll(function(){
+    let scrollint = $(this).scrollTop();
+    if ( scrollint >= 98){
+        $('#menu_index').css('display','none')
+        $('#blue_menu_index').css('display','block');
+    }else {
+        $('#menu_index').css('display','block')
+        $('#blue_menu_index').css('display','none');
+    }
+});
